392752d15c72895cca1a38235aff894f0e65c930
Chronophylos <nikolai@chronophylos.com>
2020-02-06 22:05:58 +0100

    added loggin
    Signed-off-by: Chronophylos <nikolai@chronophylos.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+#[macro_use]
+extern crate log;
+extern crate flexi_logger;
+extern crate yansi;
+
 #[tokio::main]
 async fn main() {
+    flexi_logger::Logger::with_env()
+        .format(custom_log_format)
+        .start()
+        .unwrap_or_else(|e| panic!("Logger initialization failed with {}", e));
+
     use futures::prelude::*;
 
     let (nick, pass) = (
@@ -17,6 +27,14 @@ async fn main() {
         .await
         .unwrap();
 
+    info!("creating new client");
+
+    trace!("trace");
+    debug!("debug");
+    info!("info");
+    warn!("warn");
+    error!("error");
+
     // make a client. the client is clonable
     let client = twitchchat::Client::new();
 
@@ -33,7 +51,8 @@ async fn main() {
     // spawn a task to consume the stream
     tokio::task::spawn(async move {
         while let Some(msg) = privmsg.next().await {
-            eprintln!("[{}] {}: {}", msg.channel, msg.name, msg.data);
+            trace!("Got PRIVMSG");
+            info!("[{}] {}: {}", msg.channel, msg.name, msg.data);
         }
     });
 
@@ -41,9 +60,10 @@ async fn main() {
     let mut join = dispatcher.subscribe::<twitchchat::events::Join>();
     tokio::task::spawn(async move {
         while let Some(msg) = join.next().await {
+            trace!("Got JOIN");
             // we've joined a channel
             if msg.name == nick {
-                eprintln!("you joined {}", msg.channel);
+                info!("you joined {}", msg.channel);
                 break; // returning/dropping the stream un-subscribes it
             }
         }
@@ -78,16 +98,19 @@ async fn main() {
     // dispatcher has an RAII guard, so keep it scoped
     // dropping it here so everything can proceed while keeping example brief
     drop(dispatcher);
+    trace!("dropped dispatcher");
 
+    info!("joining channel");
     // get a clonable writer from the client
     // join a channel, methods on writer return false if the client is disconnected
     if let Err(err) = client.writer().join(&channel).await {
         match err {
             twitchchat::Error::InvalidChannel(..) => {
-                eprintln!("you cannot join a channel with an empty name. demo is ending");
+                error!("could not join channel because the name is empty");
                 std::process::exit(1);
             }
             _ => {
+                error!("got an error, but I don't know what to do: {}", err);
                 // we'll get an error if we try to write to a disconnected client.
                 // if this happens, you should shutdown your tasks
             }
@@ -97,20 +120,54 @@ async fn main() {
     // await for the client to be done
     match done.await {
         Ok(twitchchat::client::Status::Eof) => {
-            eprintln!("done!");
+            info!("done!");
         }
         Ok(twitchchat::client::Status::Canceled) => {
-            eprintln!("client was stopped by user");
+            info!("client was stopped by user");
         }
         Err(err) => {
-            eprintln!("error: {}", err);
+            error!("error: {}", err);
         }
     }
 
     // note you should wait for all of your tasks to join before exiting
     // but we detached them to make this shorter
 
+    debug!("clearing twitchchat subscriptions");
     // another way would be to clear all subscriptions
     // clearing the subscriptions would close each event stream
     client.dispatcher().await.clear_subscriptions_all();
+}
+fn custom_log_format(
+    w: &mut dyn std::io::Write,
+    now: &mut flexi_logger::DeferredNow,
+    record: &flexi_logger::Record,
+) -> Result<(), std::io::Error> {
+    let level = record.level();
+    write!(
+        w,
+        " {} {:<5} {} > {}",
+        now.now().format("%Y-%m-%d %H:%M:%S"),
+        style_level(level, record.level()),
+        yansi::Paint::new(record.module_path().unwrap_or("<unnamed>")).bold(),
+        style_message(level, &record.args())
+    )
+}
+fn style_level<T>(level: log::Level, item: T) -> yansi::Paint<T> {
+    match level {
+        log::Level::Error => yansi::Paint::red(item).bold(),
+        log::Level::Warn => yansi::Paint::yellow(item).bold(),
+        log::Level::Info => yansi::Paint::green(item),
+        log::Level::Debug => yansi::Paint::blue(item),
+        log::Level::Trace => yansi::Paint::fixed(5, item),
+    }
+}
+fn style_message<T>(level: log::Level, item: T) -> yansi::Paint<T> {
+    match level {
+        log::Level::Error => yansi::Paint::red(item),
+        log::Level::Warn => yansi::Paint::yellow(item),
+        log::Level::Info => yansi::Paint::white(item),
+        log::Level::Debug => yansi::Paint::fixed(8, item),
+        log::Level::Trace => yansi::Paint::fixed(8, item).italic(),
+    }
 }
