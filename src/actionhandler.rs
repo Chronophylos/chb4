beff2bdc4a2e22835f90a69daa877fa57d33c865
Chronophylos <nikolai@chronophylos.com>
2020-02-09 15:35:01 +0100

    basic action handling
    Signed-off-by: Chronophylos <nikolai@chronophylos.com>

---
+++ src/actionhandler.rs
@@ -0,0 +1,64 @@
+/// stub
+struct Action {
+    name: String,
+    aliases: Vec<String>,
+    whitelisted: bool,
+}
+
+pub struct ActionHandler {
+    actions: Vec<Action>,
+    prefix: char,
+}
+
+impl Default for ActionHandler {
+    fn default() -> Self {
+        Self {
+            actions: vec![Action {
+                name: String::from("dummy"),
+                aliases: vec![],
+                whitelisted: true,
+            }],
+            prefix: '§',
+        }
+    }
+}
+
+impl ActionHandler {
+    /// Create a new ActionHandler
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn handle_privmsg(
+        &self,
+        msg: std::sync::Arc<twitchchat::messages::Privmsg<'_>>,
+        writer: &twitchchat::client::Writer,
+    ) {
+        let words: Vec<&str> = msg.data.trim().split_whitespace().collect();
+        let mut command = words[0].to_owned();
+        let prefix = command.remove(0);
+
+        if prefix != self.prefix {
+            trace!("Dropping message because prefix was not found");
+            return;
+        }
+
+        let args = &words[1..];
+        debug!("Command: {} Args: {:?}", command, args);
+
+        let action = self.actions.iter().find(|&a| command == a.name);
+        match action {
+            Some(a) => {
+                debug!("Found matching action {}", a.name);
+                if a.whitelisted {
+                    debug!("Executing action");
+                }
+            }
+            None => debug!("No matching action found"),
+        }
+    }
+}
+
+pub fn new() -> ActionHandler {
+    ActionHandler::new()
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate log;
 extern crate config;
 
+mod actionhandler;
 mod log_format;
 
 use twitchchat::{client::Error, client::Status, events, Client, Secure};
@@ -12,7 +13,7 @@ use tokio::stream::StreamExt as _;
 #[tokio::main]
 async fn main() {
     // should only be run once
-    flexi_logger::Logger::with_env_or_str("chb4=trace, twitchchat=debug")
+    flexi_logger::Logger::with_env_or_str("chb4=trace, debug")
         .format(log_format::format)
         .start()
         .unwrap_or_else(|e| panic!("Logger initialization failed with {}", e));
@@ -38,6 +39,10 @@ async fn main() {
         .unwrap_or_else(|e| panic!("Loading config from env failed with {}", e));
     info!("Loaded config");
 
+    let actions = actionhandler::new();
+
+    info!("Created Action Handler");
+
     let nick = settings.get_str("twitch.nick").unwrap();
     let pass = settings.get_str("twitch.pass").unwrap();
     let channel = nick.clone();
@@ -47,13 +52,7 @@ async fn main() {
         .await
         .unwrap();
 
-    info!("Creating new twitch client");
-
-    trace!("trace");
-    debug!("debug");
-    info!("info");
-    warn!("warn");
-    error!("error");
+    info!("Connected to {}", twitchchat::TWITCH_IRC_ADDRESS_TLS);
 
     // make a client. the client is clonable
     let client = Client::new();
@@ -70,7 +69,7 @@ async fn main() {
     // spawn a task to consume the stream
     tokio::task::spawn(async move {
         while let Some(msg) = privmsg.next().await {
-            trace!("Got PRIVMSG");
+            trace!("Got PRIVMSG message");
             info!("[{}] {}: {}", msg.channel, msg.name, msg.data);
         }
     });
@@ -80,10 +79,12 @@ async fn main() {
 
     tokio::task::spawn(async move {
         while let Some(msg) = join.next().await {
-            trace!("Got JOIN");
+            let writer = client.writer();
+            trace!("Got JOIN message");
             // we've joined a channel
             if msg.name == nick {
-                info!("you joined {}", msg.channel);
+                info!("Joined {}", msg.channel);
+                writer.privmsg(&msg.channel, String::from("Connected monakS"))
                 break; // returning/dropping the stream un-subscribes it
             }
         }
@@ -96,29 +97,13 @@ async fn main() {
     let bot_client = client.clone();
     tokio::task::spawn(async move {
         // get writer from cloned client so we dont move the original
-        let mut writer = bot_client.writer();
+        let writer = bot_client.writer();
         while let Some(msg) = bot.next().await {
-            match msg.data.split(" ").next() {
-                Some("!quit") => {
-                    // causes the client to shutdown
-                    bot_client.stop().await.unwrap();
-                }
-                Some("!hello") => {
-                    debug!("Got hello");
-                    let response = format!("hello {}!", msg.name);
-                    // send a message in response
-                    if let Err(err) = writer.privmsg(&msg.channel, &response).await {
-                        error!("Writing message to {} failed with {}", &msg.channel, err);
-                        // we ran into a write error, we should probably leave this task
-                        return;
-                    }
-                }
-                _ => {}
-            }
+            actions.handle_privmsg(msg, &writer);
         }
     });
 
-    info!("joining channel");
+    info!("Joining channel {}", &channel);
     // get a clonable writer from the client
     // join a channel, methods on writer return false if the client is disconnected
     if let Err(err) = client.writer().join(&channel).await {
