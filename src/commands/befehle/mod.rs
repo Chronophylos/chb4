7b5168f9efe6f454cb380dcb1586e96b58135a46
Chronophylos <nikolai@chronophylos.com>
2020-02-19 18:23:20 +0100

    make voicemail command compile but dont use it

--- src/commands/befehle/mod.rs
+++ src/commands/befehle/mod.rs
@@ -4,7 +4,12 @@ use std::sync::Arc;
 
 mod admin;
 mod test;
+mod voicemail;
 
 pub fn all(context: Arc<Context>) -> Vec<Command> {
-    vec![test::command(), admin::command(context)]
+    vec![
+        test::command(),
+        admin::command(context.clone()),
+        //voicemail::command(context.clone()),
+    ]
 }
--- src/commands/befehle/voicemail.rs
+++ src/commands/befehle/voicemail.rs
@@ -1,10 +1,10 @@
-use crate::command::{Command, CommandResult};
+use crate::commands::command::{Command, CommandResult};
 use crate::context::Context;
 use std::sync::Arc;
 
-pub fn command<'a>(context: Arc<Context>) -> Command<'a> {
+pub fn command(_context: Arc<Context>) -> Command {
     Command::with_name("voicemail")
-        .aliases(vec!["tell"])
-        .command(|bot, args| {})
+        .aliases(vec![String::from("tell")])
+        .command(|_args, _msg| CommandResult::Error(String::from("Not implemented")))
         .done()
 }
