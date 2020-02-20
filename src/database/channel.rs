8481bcbfb34c49c16b535b7a735a7ad378982901
Chronophylos <nikolai@chronophylos.com>
2020-02-20 17:19:16 +0100

    comments and fix get_channel not returning anything

--- src/database/channel.rs
+++ src/database/channel.rs
@@ -4,13 +4,44 @@ use diesel::prelude::*;
 use diesel::MysqlConnection;
 
 pub fn get_channel<'a>(conn: &MysqlConnection, name: &'a str) {
-    use user::get_user_by_name;
+    use super::get_user_by_name;
 
-    let user = get_user_by_name(name);
+    let user = get_user_by_name(conn, name);
 
     Channel::belonging_to(&user)
+        .get_result(conn)
+        .expect("Could not get channel belonging to user")
 }
 
+/// Join a channel with `name`. If the channel already exists simply update `enabled` to `true`
+/// regardless of it's current value. If the channel does not exists check if a user with `name`
+/// exists and create a new channel pointing to that user and set enabled to `true`. If no user
+/// exists create a new user with only the name and continue with creating the channel.
+///
+/// See below for a illustration:
+///
+/// ```markdeep
+///                                                               .-----------------------.
+///                                                               | Set enabled to `true` |
+///                                                               '-----------------------'
+///                                                                          ^
+///                                                                          |
+///                                .                                         .
+///                               / \                                       / \
+///                              /   \                                     /   \
+/// .----------------------.    /     \  Y  .------------------------.    /     \
+/// | Get user with `name` +-->+Exists?+--->| Get associated channel +-->+Exists?+
+/// '----------------------'    \     /     '------------------------'    \     /
+///                              \   /                   ^                 \   /
+///                               \ /                    |                  \ /
+///                                .                     |                   .
+///                                |N                    |                   |N
+///                                v                     |                   v
+///                     .-------------------------.      |      .---------------------------.
+///                     | Create user with `name` +------+      | Create channel of user    |
+///                     '-------------------------'             | and set enabled to `true` |
+///                                                             '---------------------------'
+/// ```
 pub fn join_channel<'a>(conn: &MysqlConnection, name: &'a str) {
     let channel = get_channel(conn, name);
 
--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,3 +1,5 @@
+mod channel;
 mod user;
 
+pub use channel::*;
 pub use user::*;
