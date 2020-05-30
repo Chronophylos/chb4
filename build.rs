4889d7e611f4490241f57df299e9c2d8cd49ab17
Chronophylos <nikolai@chronophylos.com>
2020-05-30 22:58:17 +0200

    Revert "use rev-pase instead of show-ref to get current commit"
    This reverts commit 8b2fd8185c9b74134ca341e9933892e3ce6d01f3.

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     let output = Command::new("git")
-        .args(&["ref-parse", "--short", "HASH"])
+        .args(&["show-ref", "--hash", "--abbrev"])
         .output()
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
